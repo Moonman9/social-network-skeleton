bda6e77fc6f93556576b2e126fef7e317cd737df
Moonman9 <zzepold97@gmail.com>
2022-06-04 15:52:17 -0700

    get and create users

---
+++ controllers/user-controller.js
@@ -0,0 +1,21 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers(req, res)
+    {
+        User.find({})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+    addNewUser({ body }, res)
+    {
+        User.create(body)
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => res.json(err));
+    },
+}
+
+module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
+const {
+    getAllUsers,
+    addNewUser
+} = require('../../controllers/user-controller');
+
 // GET and POST routes for users
 router
     .route('/')
     .get(getAllUsers)
     .post(addNewUser);
+
+module.exports = router;
